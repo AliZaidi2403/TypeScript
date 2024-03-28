c29ab542f185af15e49737263b62fa125ba78446
Amaan <amaanzaidi462@gmail.com>
2024-03-28 21:19:07 +0530

    Lesson1

---
+++ Lesson1/build/js/main.js
@@ -0,0 +1,7 @@
+"use strict";
+let username = "Amaan";
+console.log(username);
+let a = 12;
+let b = "3";
+let c = 5;
+console.log(a * b);
---
+++ Lesson2/src/main.ts
@@ -0,0 +1,7 @@
+let username = "Amaan";
+console.log(username);
+
+let a: number = 12;
+let b: number = 3;
+let c: number = 5;
+console.log(a * b);
