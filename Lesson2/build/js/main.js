cb2b46eb01a9d1a52d83f87f1d76476d8185cd3a
Amaan <amaanzaidi462@gmail.com>
2024-03-28 21:54:01 +0530

    datatypes

--- Lesson2/build/js/main.js
+++ Lesson2/build/js/main.js
@@ -1,7 +1,25 @@
 "use strict";
-let username = "Amaan";
-console.log(username);
-let a = 12;
-let b = "3";
-let c = 5;
-console.log(a * b);
+let a = "amaan";
+// error since you can only assign a to string a = 6;
+let meaningOfLife;
+let isLoading;
+let album; //this means we can store any type of value in here
+let win; // win can be either number or string
+a = "united";
+// error :  isLoading = 5;
+isLoading = true;
+meaningOfLife = 22;
+album = 10;
+album = "One Direction";
+/*const sum = (a, b) => {
+  return a + b;
+};*/
+//here the error occur because typescript is not able to infer the type of a and b so it is assiging
+// any type to both of them because the plus symbol can be used for both addition and concatenation
+const sum1 = (a, b) => {
+    return a + b;
+};
+//here typescript can infer input parameter can be number and string and output wil be string
+const sum2 = (a, b) => {
+    return a + b;
+};
--- Lesson2/src/main.ts
+++ Lesson2/src/main.ts
@@ -1,7 +1,27 @@
-let username = "Amaan";
-console.log(username);
+let a = "amaan";
+// error since you can only assign a to string a = 6;
+let meaningOfLife: number;
+let isLoading: boolean;
+let album: any; //this means we can store any type of value in here
+let win: string | number; // win can be either number or string
+a = "united";
+// error :  isLoading = 5;
+isLoading = true;
+meaningOfLife = 22;
+album = 10;
+album = "One Direction";
 
-let a: number = 12;
-let b: number = 3;
-let c: number = 5;
-console.log(a * b);
+/*const sum = (a, b) => {
+  return a + b;
+};*/
+//here the error occur because typescript is not able to infer the type of a and b so it is assiging
+// any type to both of them because the plus symbol can be used for both addition and concatenation
+
+const sum1 = (a: number, b: string) => {
+  return a + b;
+};
+//here typescript can infer input parameter can be number and string and output wil be string
+
+const sum2 = (a: number, b: number) => {
+  return a + b;
+};
